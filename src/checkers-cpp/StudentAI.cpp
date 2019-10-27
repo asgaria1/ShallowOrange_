65a205cfa4aa280577a3e136d3e048722f4c6767
Kuixi Ren <kuixir@uci.edu>
2019-10-26 18:50:31 -0700

    continued on minimax

--- src/checkers-cpp/StudentAI.cpp
+++ src/checkers-cpp/StudentAI.cpp
@@ -13,7 +13,7 @@ StudentAI::StudentAI(int col,int row,int p)
 
 Move StudentAI::GetMove(Move move)
 {
-    int curr = 0;
+    int curr = INT_MIN;
     Move bestMove;
     if (move.seq.empty())
     {
@@ -24,8 +24,14 @@ Move StudentAI::GetMove(Move move)
     vector<vector<Move> > moves = board.getAllPossibleMoves(player);
     for (int i = 0; i <= moves.size(); i++){
         for (int j = 0; j <= moves[i].size(); j++){
-            //make move on copy of board so u can pass it min
-            curr = basic_minimax(board, player, 4, "min");
+            //make move on board so u can pass it to minimax function
+            board.makeMove(moves[i][j], player);
+            int result = basic_minimax(board, player, 4, "min"); //will return a number for this move, the higher the better
+            if (result > curr) {
+                curr = result;
+                bestMove = moves[i][j];
+            }
+            board.Undo(); //undo change in order to test another move
         }
     }
     //int i = rand() % (moves.size());
@@ -33,8 +39,8 @@ Move StudentAI::GetMove(Move move)
     //int j = rand() % (checker_moves.size());
     //Move res = checker_moves[j];
     //board.makeMove(res,player);
-    cout<<getScore(board)<<endl;
-    return res;
+    //cout<<getScore(board)<<endl;
+    //return res;
 
 
 }
@@ -62,14 +68,54 @@ int StudentAI::basic_minimax(Board board, int player, int depth, string state){
     //int curr_score = getScore(board, player);
     //std::pair move_pair = std::pair(getScore(board, player), move??)
 
+    //the board object do not know whose turn it is, its makeMove simply takes a player argument and act the move
+    //for that player. The turn order is maintained by GameLogic.
+    //Therefore, in order to make the move for the right player on each recursive level, we need to check whether if
+    //its min or max's turn. since for this function, player is initial player that started this function. If state is
+    //max then the current player taking the turn is the same as player, else it is the opponent.. Another way to do this
+    //is to change it to the right player when doing the recursive call, this way we just have to check when we do the final
+    //evaluation...
     if (depth == 0) {
+
         return getScore(board, player);
     }
-    if max:
-        get alll motsves
-        cur best = stdpair(sdasd)
-        for aeach nvoemoves
-            make move on copy of board:
-            let x = basic_miiamax(thisnewboard, player , deptht -1, max if this is min
-            if x[0] > best[0] best = x
+    int thisTurnPlayer;
+    int curBestValue;//keeping track of the best move I can make, if im a min then the lower the better, if im
+                        //a max then higher the better
+    if (state=="max"){
+        thisTurnPlayer = player; //since state is max then the player moving this turn is the same as the init player
+        curBestValue = INT_MIN;
+    }
+    else{
+        thisTurnPlayer = player == 1? 2:1; //reverse if state is min
+        curBestValue = INT_MAX;
+    }
+
+    vector<vector<Move> > moves = board.getAllPossibleMoves(thisTurnPlayer); //get the moves for the player moving this turn
+
+    for (int i = 0; i <= moves.size(); i++){
+        for (int j = 0; j <= moves[i].size(); j++){
+            //make move on board so u can pass it to minimax function
+            board.makeMove(moves[i][j], thisTurnPlayer);
+            string nextState = state == "min"? "max":"min";
+            int result = basic_minimax(board, player, 4, state); //will return a number for this move, the higher the better
+            //=================================== I go EATTT
+            if (result > curr) {
+                curr = result;
+                bestMove = moves[i][j];
+            }
+            board.Undo(); //undo change in order to test another move
+        }
+    }
+
+
+        //        get alll motsves
+//        cur best = stdpair(sdasd)
+//        for aeach nvoemoves
+//            make move on copy of board:
+//            let x = basic_miiamax(thisnewboard, player , deptht -1, max if this is min
+//            if x[0] > best[0] best = x
+
+    }
+
 }
