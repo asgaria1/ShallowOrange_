4b26e6cdf8d04dfe163fa95dd9d9353e8bd45504
Ari <ariannaasgari@gmail.com>
2019-10-26 19:41:36 -0700

    seg fault minimax

--- src/checkers-cpp/StudentAI.cpp
+++ src/checkers-cpp/StudentAI.cpp
@@ -19,7 +19,7 @@ Move StudentAI::GetMove(Move move)
     {
         player = 1;
     } else{
-        board.makeMove(move,player == 1?2:1);
+        //board.makeMove(move,player == 1?2:1);
     }
     vector<vector<Move> > moves = board.getAllPossibleMoves(player);
     for (int i = 0; i <= moves.size(); i++){
@@ -34,6 +34,7 @@ Move StudentAI::GetMove(Move move)
             board.Undo(); //undo change in order to test another move
         }
     }
+    return bestMove;
     //int i = rand() % (moves.size());
     //vector<Move> checker_moves = moves[i];
     //int j = rand() % (checker_moves.size());
@@ -65,9 +66,6 @@ int StudentAI::getScore(Board board, int player)
 }
 
 int StudentAI::basic_minimax(Board board, int player, int depth, string state){
-    //int curr_score = getScore(board, player);
-    //std::pair move_pair = std::pair(getScore(board, player), move??)
-
     //the board object do not know whose turn it is, its makeMove simply takes a player argument and act the move
     //for that player. The turn order is maintained by GameLogic.
     //Therefore, in order to make the move for the right player on each recursive level, we need to check whether if
@@ -80,8 +78,10 @@ int StudentAI::basic_minimax(Board board, int player, int depth, string state){
         return getScore(board, player);
     }
     int thisTurnPlayer;
-    int curBestValue;//keeping track of the best move I can make, if im a min then the lower the better, if im
-                        //a max then higher the better
+    int curBestValue;//keeping track of the best val of move I can make, if im a min then the lower the better, if im
+                    //a max then higher the better
+    Move bestMove; //keeping track of the actual best move to be made
+
     if (state=="max"){
         thisTurnPlayer = player; //since state is max then the player moving this turn is the same as the init player
         curBestValue = INT_MIN;
@@ -98,24 +98,22 @@ int StudentAI::basic_minimax(Board board, int player, int depth, string state){
             //make move on board so u can pass it to minimax function
             board.makeMove(moves[i][j], thisTurnPlayer);
             string nextState = state == "min"? "max":"min";
-            int result = basic_minimax(board, player, 4, state); //will return a number for this move, the higher the better
-            //=================================== I go EATTT
-            if (result > curr) {
-                curr = result;
-                bestMove = moves[i][j];
+            int result = basic_minimax(board, player, depth -1, nextState); //will return a number for this move, the higher the better
+                            //passed depth -1 for recursive base case + passed next state instead of state because u want to
+                            //recursively call the next state, not the current state
+            //=================================== kuixi break
+            if ( state == "min" ){
+                if (result < curBestValue) {
+                    curBestValue = result;
+                    bestMove = moves[i][j];
+                }
+            }else { // state == "max"
+                if (result > curBestValue) {
+                    curBestValue = result;
+                    bestMove = moves[i][j];
+                }
             }
             board.Undo(); //undo change in order to test another move
         }
     }
-
-
-        //        get alll motsves
-//        cur best = stdpair(sdasd)
-//        for aeach nvoemoves
-//            make move on copy of board:
-//            let x = basic_miiamax(thisnewboard, player , deptht -1, max if this is min
-//            if x[0] > best[0] best = x
-
-    }
-
 }
